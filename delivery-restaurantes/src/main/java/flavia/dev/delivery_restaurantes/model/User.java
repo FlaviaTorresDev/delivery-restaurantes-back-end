97e83560ca7a308f4f815b26f396a11aae680e68
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-08-12 15:19:55 -0300

    criado classe de 'user' e 'Ordem'

---
+++ delivery-restaurantes/src/main/java/flavia/dev/delivery_restaurantes/model/User.java
@@ -0,0 +1,44 @@
+package flavia.dev.delivery_restaurantes.model;
+
+import java.util.List;
+
+import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String sobrenome;
+	private String email;
+	private String password;
+
+	private USER_ROLE role;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+	private List<Order> orders;
+
+	@ElementCollection
+	private List<RestaurantDto> favorites=new ArrayList<>();
+	
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Address> addresses = new ArrayList<>();
+	
+	private String status;
+
+}
