fdb87d52f177baf783e5b507b1596965b9d676d1
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-08-12 16:14:10 -0300

    criada a classe 'endereço'

--- delivery-restaurantes/src/main/java/flavia/dev/delivery_restaurantes/model/Endereco.java
+++ delivery-restaurantes/src/main/java/flavia/dev/delivery_restaurantes/model/Endereco.java
@@ -1,8 +1,35 @@
 package flavia.dev.delivery_restaurantes.model;
 
+
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Endereco {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String numero;
+
+	private String rua;
+
+	private String cidade;
+
+	private String estado;
+
+	private String cep;
+
+	private String pais;
 
 }
+
