6a433aea8b3fa8996b0c750e2f0103341700eccf
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-08-13 10:43:36 -0300

    tabelas de review e informações de contato criadas

--- delivery-restaurantes/src/main/java/flavia/dev/delivery_restaurantes/model/InformacaoContato.java
+++ delivery-restaurantes/src/main/java/flavia/dev/delivery_restaurantes/model/InformacaoContato.java
@@ -1,5 +1,20 @@
 package flavia.dev.delivery_restaurantes.model;
 
-public class InformacaoContato {
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InformacaoContato {
+	
+    private String email;
+    private String telefone;
+    private String twitter;
+    private String instagram;
+
+   
 }
--- delivery-restaurantes/src/main/java/flavia/dev/delivery_restaurantes/model/Review.java
+++ delivery-restaurantes/src/main/java/flavia/dev/delivery_restaurantes/model/Review.java
@@ -1,5 +1,40 @@
 package flavia.dev.delivery_restaurantes.model;
 
-public class Review {
+import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User cliente;
+
+    @ManyToOne
+    @JsonIgnore
+    private Restaurant restaurant;
+    
+    private String mensagem;
+    
+    private double avaliacao;
+
+    private LocalDateTime criadoEm;
+    
+   
 }
+
