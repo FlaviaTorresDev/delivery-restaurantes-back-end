af2bf2decfa5cd45e3813a8cb199bf7830740711
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-08-21 16:15:31 -0300

    configurando as classes de configuração

---
+++ delivery-restaurantes/src/main/java/flavia/dev/delivery_restaurantes/config/JwtConstant.java
@@ -0,0 +1,9 @@
+package flavia.dev.delivery_restaurantes.config;
+
+public class JwtConstant {
+
+
+	public static final String SECRET_KEY="wpembytrwcvnryxksdbqwjebruyGHyudqgwveytrtrCSnwifoesarjbwe";
+	public static final String JWT_HEADER="Authorization";
+	
+}
